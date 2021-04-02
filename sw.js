d59ce5f821a59d2b03ad6fb2ea9edf3d203900ab
leuterio <jeffersonmoraes@live.com>
2021-04-02 10:51:58 -0300

    custom install button

--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
+buttonInstall = document.querySelector(".install");
 // Set a name for the current cache
 var cacheName = 'v1'; 
 
@@ -12,10 +13,76 @@ var cacheFiles = [
     'https://kit.fontawesome.com/e8f2de5d88.js'
 ]
 
+// Initialize deferredPrompt for use later to show browser install prompt.
+let deferredPrompt;
+
 window.addEventListener('beforeinstallprompt', (e) => {
-    // Prevent Chrome 67 and earlier from automatically showing the prompt
-    e.preventDefault();
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can install the PWA
+  showInstallPromotion();
+  // Optionally, send analytics event that PWA install promo was shown.
+  console.log(`'beforeinstallprompt' event was fired.`);
 });
+
+buttonInstall.addEventListener('click', async () => {
+    // Hide the app provided install promotion
+    hideInstallPromotion();
+    // Show the install prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    const { outcome } = await deferredPrompt.userChoice;
+    // Optionally, send analytics event with outcome of user choice
+    console.log(`User response to the install prompt: ${outcome}`);
+    // We've used the prompt, and can't use it again, throw it away
+    deferredPrompt = null;
+  });
+
+  window.addEventListener('appinstalled', () => {
+    // Hide the app-provided install promotion
+    hideInstallPromotion();
+    // Clear the deferredPrompt so it can be garbage collected
+    deferredPrompt = null;
+    // Optionally, send analytics event to indicate successful install
+    console.log('PWA was installed');
+  });
+
+self.addEventListener('install', function(e) {
+    // e.waitUntil Delays the event until the Promise is resolved
+    e.waitUntil(
+
+    	// Open the cache
+	    caches.open(cacheName).then(function(cache) {
+
+	    	// Add all the default files to the cache
+			return cache.addAll(cacheFiles);
+	    })
+	); // end e.waitUntil
+});
+
+
+self.addEventListener('activate', function(e) {
+
+    e.waitUntil(
+
+    	// Get all the cache keys (cacheName)
+		caches.keys().then(function(cacheNames) {
+			return Promise.all(cacheNames.map(function(thisCacheName) {
+
+				// If a cached item is saved under a previous cacheName
+				if (thisCacheName !== cacheName) {
+
+					// Delete that cached file
+					return caches.delete(thisCacheName);
+				}
+			}));
+		})
+	); // end e.waitUntil
+
+});
+
 
 self.addEventListener('fetch', function(e) {
 
